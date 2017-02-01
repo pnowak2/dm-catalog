fa3b8faf293a6240073d921331023fd5f446e13a
Piotr Nowak <p.nowak2@gmail.com>
2017-02-01 19:10:46 +0100

    members count.

--- src/app/family-composition-container/family-composition-container.component.ts
+++ src/app/family-composition-container/family-composition-container.component.ts
@@ -10,10 +10,12 @@ export class FamilyCompositionContainerComponent implements AfterContentInit {
   @Input() opened: boolean;
   @ContentChildren(FamilyMemberComponent) members: QueryList<FamilyMemberComponent>;
   selectedPerson: string;
+  membersCount: number;
   
   constructor() { }
 
   ngAfterContentInit() {
+    this.membersCount = this.members.length;
     this.members.toArray().forEach((member: FamilyMemberComponent) => {
       member.clicked.subscribe((m: FamilyMemberComponent) => {
         this.members.toArray().forEach((member: FamilyMemberComponent) => member.active = false);
