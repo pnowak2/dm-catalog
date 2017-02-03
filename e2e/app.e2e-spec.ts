6b0bfd9825872129c157fe553fa1df63e4f53905
Piotr Nowak <p.nowak2@gmail.com>
2017-02-03 16:28:24 +0100

    e2e test running.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('pgd App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toContain('Dirk');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,6 @@ export class PgdPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('.asm-family-member')).getText();
   }
 }
