09a4ed0e6bccdba26b6697e9eb82ede44486de97
Piotr Nowak <p.nowak2@gmail.com>
2017-02-10 14:32:48 +0100

    app component sample data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,38 +7,10 @@ import { FamilyMemberViewModel, Coverage, Sex } from './components/family-bar/fa
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  familyMember: FamilyMemberViewModel = {};
+  familyMembers: Array<FamilyMemberViewModel> = [{
+    firstName: 'Piotr'
+  }];
 
   md: Date = new Date();
   title: 'app works!';
-
-  constructor() {
-    this.familyMember.firstName = 'Piotr';
-    this.familyMember.familyName = 'Nowak';
-    this.familyMember.mainLanguage = 'pol';
-    this.familyMember.sex = Sex.Male;
-    this.familyMember.birthDate = '1986/04/06';
-    this.familyMember.deathDate = '2004/12/16';
-    this.familyMember.delegationCountry = 'BEL';
-    this.familyMember.disabled = false;
-    this.familyMember.personalNumber = '346001';
-    this.familyMember.relationName = 'Affiliate';
-    this.familyMember.selected = true;
-    this.familyMember.hasComments = true;
-    this.familyMember.coveredByOtherAffiliate = true;
-    this.familyMember.accidentCoverage = Coverage.Full;
-    this.familyMember.sicknessCoverage = Coverage.Complementary;
-  }
-
-  @HostListener('document:click', ['$event'])
-  clickout(event) {
-    // this.familyMember.disabled = !this.familyMember.disabled;
-    // this.familyMember.selected = !this.familyMember.selected;
-    // this.familyMember.coveredByOtherAffiliate = !this.familyMember.coveredByOtherAffiliate;
-    // this.familyMember.sicknessCoverage = Coverage.Full;
-    // this.familyMember.language = null;
-    // this.familyMember.country = null;
-    // this.familyMember.hasComments = false;
-  }
-
 }
