997e848ff4ca53a0c63dfcaee9c27092ee6484ac
Piotr Nowak <p.nowak2@gmail.com>
2017-03-02 16:48:30 +0100

    done

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'asm-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class SidebarComponent implements OnInit {
