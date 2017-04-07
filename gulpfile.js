6420700f14bf5491aae9462bc50e79e7afc19748
Piotr Nowak <p.nowak2@gmail.com>
2017-04-07 16:08:10 +0200

    added gulp support.

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sassdoc = require('sassdoc');
+
+var inputFolder = 'scss/';
+var input = inputFolder + 'dm.scss';
+var output = 'dist';
+
+const sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+const autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR'],
+  cascade: false
+}
+
+const sassdocOptions = {
+  dest: './public/sassdoc'
+};
+
+gulp.task('sass', () =>
+  gulp.src(input)
+    .pipe(sass(sassOptions))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(output))
+);
+
+gulp.task('sassdoc', function () {
+  return gulp
+    .src(inputFolder)
+    .pipe(sassdoc(sassdocOptions))
+    .resume();
+});
+
+gulp.task('watch', function () {
+  return gulp
+    .watch(inputFolder + '/**', ['sass'])
+    .on('change', function (event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+gulp.task('default', ['sass', 'watch']);
