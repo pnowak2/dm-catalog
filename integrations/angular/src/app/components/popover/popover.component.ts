c996ba32f78d53fcbc1f4d221abfe3b69fc5c967
Piotr Nowak <p.nowak2@gmail.com>
2017-06-19 11:09:31 +0200

    title and content support.

--- integrations/angular/src/app/components/popover/popover.component.ts
+++ integrations/angular/src/app/components/popover/popover.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'dm-popover',
   templateUrl: './popover.component.html'
 })
 export class PopoverComponent implements OnInit {
+  @Input() title;
 
   constructor() { }
 
