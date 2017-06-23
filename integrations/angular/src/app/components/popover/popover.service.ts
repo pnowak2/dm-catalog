36daff0e92a91a20391a4ac887525452950fe88a
Piotr Nowak <p.nowak2@gmail.com>
2017-06-23 09:07:07 +0200

    started refactor and simplification.

--- integrations/angular/src/app/components/popover/popover.service.ts
+++ integrations/angular/src/app/components/popover/popover.service.ts
@@ -4,6 +4,9 @@ import * as mezr from 'mezr';
 @Injectable()
 export class PopoverService {
   position(desiredPlacement: string, popoverContainerElement: HTMLElement, triggerElement: HTMLElement, popoverArrow: HTMLElement): void {
+    this.clearPlacementCss(popoverContainerElement);
+    this.addPlacementCss(desiredPlacement, popoverContainerElement);
+
     let effectivePosition = this.getEffectivePosition(
       desiredPlacement,
       popoverContainerElement,
@@ -11,34 +14,11 @@ export class PopoverService {
       popoverArrow
     );
 
-    popoverContainerElement.style.top = effectivePosition.top + 'px';
-    popoverContainerElement.style.left = effectivePosition.left + 'px';
+    this.updateContainerCoordinates(popoverContainerElement, effectivePosition);
   }
 
   getEffectivePosition(desiredPlacement: string, popoverContainerElement: HTMLElement, triggerElement: HTMLElement, popoverArrow: HTMLElement): { top: number, left: number } {
-    let position;
-    let offsetY = 0;
-    let offsetX = 0;
-
-    popoverContainerElement.classList.remove('dm-c-popover--top');
-    popoverContainerElement.classList.remove('dm-c-popover--bottom');
-    popoverContainerElement.classList.remove('dm-c-popover--left');
-    popoverContainerElement.classList.remove('dm-c-popover--right');
-    popoverContainerElement.classList.add(`dm-c-popover--${desiredPlacement}`);
-
-    if (desiredPlacement === 'bottom') {
-      position = 'center top center bottom';
-      offsetY = 15;
-    } else if (desiredPlacement === 'top') {
-      position = 'center bottom center top';
-      offsetY = -15;
-    } else if (desiredPlacement === 'left') {
-      position = 'right center left center';
-      offsetX = -15;
-    } else if (desiredPlacement === 'right') {
-      position = 'left center right center';
-      offsetX = 15;
-    }
+    let { position, offsetX, offsetY } = this.calculatePositionConfiguration(desiredPlacement);
 
     // should have different strategies for onOverflow while top/bottom or left/right
     return mezr.place({
@@ -114,4 +94,45 @@ export class PopoverService {
     });
   }
 
+  clearPlacementCss(popoverContainerElement: HTMLElement): void {
+    popoverContainerElement.classList.remove('dm-c-popover--top');
+    popoverContainerElement.classList.remove('dm-c-popover--bottom');
+    popoverContainerElement.classList.remove('dm-c-popover--left');
+    popoverContainerElement.classList.remove('dm-c-popover--right');
+  }
+
+  calculatePositionConfiguration(desiredPlacement: string): { position: string, offsetX: number, offsetY: number } {
+    let position;
+    let offsetY = 0;
+    let offsetX = 0;
+
+    if (desiredPlacement === 'bottom') {
+      position = 'center top center bottom';
+      offsetY = 15;
+    } else if (desiredPlacement === 'top') {
+      position = 'center bottom center top';
+      offsetY = -15;
+    } else if (desiredPlacement === 'left') {
+      position = 'right center left center';
+      offsetX = -15;
+    } else if (desiredPlacement === 'right') {
+      position = 'left center right center';
+      offsetX = 15;
+    }
+
+    return {
+      position,
+      offsetX,
+      offsetY
+    }
+  }
+
+  addPlacementCss(desiredPlacement: string, popoverContainerElement: HTMLElement): void {
+    popoverContainerElement.classList.add(`dm-c-popover--${desiredPlacement}`);
+  }
+
+  updateContainerCoordinates(popoverContainerElement: HTMLElement, effectivePosition: { top: number, left: number }): void {
+    popoverContainerElement.style.top = effectivePosition.top + 'px';
+    popoverContainerElement.style.left = effectivePosition.left + 'px';
+  }
 }
