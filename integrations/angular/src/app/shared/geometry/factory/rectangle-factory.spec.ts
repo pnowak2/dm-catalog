cd98540b830e0ef5a8e596144976812c72af3741
Piotr Nowak <p.nowak2@gmail.com>
2017-07-31 16:14:32 +0200

    finished rect factory.

--- integrations/angular/src/app/shared/geometry/factory/rectangle-factory.spec.ts
+++ integrations/angular/src/app/shared/geometry/factory/rectangle-factory.spec.ts
@@ -29,18 +29,56 @@ fdescribe('RectangleFactory', () => {
 
         expect(rect).toEqual(Rectangle.create(14, 18, 27, 39));
       });
-
     });
 
     describe('RectangleFactory.fromSvgElement()', () => {
       it('should be defined', () => {
         expect(RectangleFactory.fromSvgElement).toEqual(jasmine.any(Function));
       });
+
+      it('should return rectangle from Svg element', () => {
+        spyOn(RectangleFactory, 'getWindow').and.returnValue({
+          pageXOffset: 12,
+          pageYOffset: 17
+        });
+
+        const fakeEl = {
+          clientWidth: 27,
+          clientHeight: 39,
+          getBoundingClientRect() {
+            return {
+              left: 2,
+              top: 1
+            }
+          }
+        } as SVGElement;
+
+        const rect = RectangleFactory.fromSvgElement(fakeEl);
+
+        expect(rect).toEqual(Rectangle.create(14, 18, 27, 39));
+      });
     });
 
     describe('RectangleFactory.fromWindow()', () => {
       it('should be defined', () => {
         expect(RectangleFactory.fromWindow).toEqual(jasmine.any(Function));
+      });
+
+      it('should return rectangle from window', () => {
+        spyOn(RectangleFactory, 'getWindow').and.returnValue({
+          pageXOffset: 12,
+          pageYOffset: 17,
+          document: {
+            documentElement: {
+              clientWidth: 1280,
+              clientHeight: 800,
+            }
+          }
+        });
+
+        const rect = RectangleFactory.fromWindow();
+
+        expect(rect).toEqual(Rectangle.create(12, 17, 1280, 800));
       });
     });
 
