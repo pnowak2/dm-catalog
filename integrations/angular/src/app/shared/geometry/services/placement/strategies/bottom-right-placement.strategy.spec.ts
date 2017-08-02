b5d4795ab4e53c6d173125109378038b873d9977
Piotr Nowak <p.nowak2@gmail.com>
2017-08-02 20:04:38 +0200

    bottom right strategy

---
+++ integrations/angular/src/app/shared/geometry/services/placement/strategies/bottom-right-placement.strategy.spec.ts
@@ -0,0 +1,99 @@
+import { PlacementOptions } from './../../../interface/placement-options';
+import { Rectangle } from './../../../model/rectangle';
+import { PlacementStrategy } from './../../../interface/placement-strategy';
+import { BottomRightPlacementStrategy } from './bottom-right-placement.strategy';
+
+describe('BottomRightPlacementStrategy', () => {
+  describe('Api', () => {
+    describe('.getId()', () => {
+      it('should be defined', () => {
+        expect(BottomRightPlacementStrategy.prototype.getId).toEqual(jasmine.any(Function));
+      });
+
+      it('should return proper id', () => {
+        const str = new BottomRightPlacementStrategy();
+        expect(str.getId()).toEqual('bottom-right');
+      });
+    });
+
+    describe('.calculate()', () => {
+      let str: PlacementStrategy;
+
+      beforeEach(() => {
+        str = new BottomRightPlacementStrategy();
+      });
+
+      it('should be defined', () => {
+        expect(BottomRightPlacementStrategy.prototype.calculate).toEqual(jasmine.any(Function));
+      });
+
+      it('should clone element', () => {
+        const anchor = Rectangle.create(1, 1, 4, 3);
+        const element = Rectangle.create(0, 0, 6, 2);
+
+        const options = {};
+        const calculatedRect = str.calculate(anchor, element, options);
+
+        expect(calculatedRect).not.toBe(element);
+      });
+
+      it('should move element to right center with offset', () => {
+        const anchor = Rectangle.create(5, 4, 3, 2);
+        const element = Rectangle.create(0, 0, 4, 3);
+        const parent = Rectangle.create(0, 0, 12, 10);
+
+        const options = {
+          offset: 2,
+          flip: false,
+          constrainToParent: false,
+          parent: parent
+        };
+
+        const calculatedRect = str.calculate(anchor, element, options);
+
+        expect(calculatedRect).toEqual(Rectangle.create(10, 3.5, 4, 3));
+      });
+
+      it('should move element to right center with offset & flip', () => {
+        const anchor = Rectangle.create(5, 4, 3, 2);
+        const element = Rectangle.create(0, 0, 4, 3);
+        const parent = Rectangle.create(0, 0, 12, 10);
+
+        const options = {
+          offset: 2,
+          flip: true,
+          constrainToParent: false,
+          parent: parent
+        };
+
+        const calculatedRect = str.calculate(anchor, element, options);
+
+        expect(calculatedRect).toEqual(Rectangle.create(-1, 3.5, 4, 3));
+      });
+
+      it('should move element to right center with offset & flip & constrain', () => {
+        const anchor = Rectangle.create(5, 4, 3, 2);
+        const element = Rectangle.create(0, 0, 4, 3);
+        const parent = Rectangle.create(0, 0, 12, 10);
+
+        const options = {
+          offset: 2,
+          flip: true,
+          constrainToParent: true,
+          parent: parent
+        };
+
+        const calculatedRect = str.calculate(anchor, element, options);
+
+        expect(calculatedRect).toEqual(Rectangle.create(0, 3.5, 4, 3));
+      });
+    });
+  });
+});
+
+function makePlacementStrategy(id: string, placedRect: Rectangle): PlacementStrategy {
+  return {
+    getId: jasmine.createSpy('id spy').and.returnValue(id),
+    calculate: jasmine.createSpy('calculate spy').and.returnValue(placedRect)
+  }
+}
