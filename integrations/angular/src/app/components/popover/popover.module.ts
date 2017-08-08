8d3ff82520de6d992060526728a8d1a5ce326de0
Piotr Nowak <p.nowak2@gmail.com>
2017-08-08 15:46:36 +0200

    only interested parties asking for geometry module.

--- integrations/angular/src/app/components/popover/popover.module.ts
+++ integrations/angular/src/app/components/popover/popover.module.ts
@@ -1,11 +1,12 @@
-import { PopoverService } from './services/popover.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PopoverService } from './services/popover.service';
+import { GeometryModule } from './../../shared/geometry/geometry.module';
 
 import { PopoverComponent } from './popover.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, GeometryModule],
   exports: [PopoverComponent],
   declarations: [PopoverComponent],
   providers: [PopoverService]
