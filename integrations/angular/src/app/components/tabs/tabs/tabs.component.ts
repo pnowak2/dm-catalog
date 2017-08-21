73dbf3d5357614a7560929f729d8f4f50ea39c17
Piotr Nowak <p.nowak2@gmail.com>
2017-08-21 21:46:45 +0200

    scss files

--- integrations/angular/src/app/components/tabs/tabs/tabs.component.ts
+++ integrations/angular/src/app/components/tabs/tabs/tabs.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit, AfterViewInit, ContentChildren, QueryList } from '@angular/core';
+import { Component, AfterViewInit, ContentChildren, QueryList, Optional } from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'dm-tabs',
   templateUrl: './tabs.component.html'
 })
-export class TabsComponent implements OnInit, AfterViewInit {
+export class TabsComponent implements AfterViewInit {
   @ContentChildren(TabComponent) tabs:QueryList<TabComponent>;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   ngAfterViewInit() {
     this.tabs.forEach((tab) => {
