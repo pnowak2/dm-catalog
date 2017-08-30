fb9ad7bfd82ce9cb34f3e9c31d6962bd757e4296
Piotr Nowak <p.nowak2@gmail.com>
2017-08-30 15:21:25 +0200

    custom templates.

--- integrations/angular/src/app/components/menu/menu-item/menu-item.component.ts
+++ integrations/angular/src/app/components/menu/menu-item/menu-item.component.ts
@@ -1,23 +1,35 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, AfterContentInit, ViewChild, Input, EventEmitter, Output, ContentChild, TemplateRef, ViewContainerRef } from '@angular/core';
 import { MenuItem } from './model/menu-item';
 
 @Component({
   selector: 'dm-menu-item',
   templateUrl: './menu-item.component.html'
 })
-export class MenuItemComponent implements OnInit {
+export class MenuItemComponent implements OnInit, AfterContentInit {
   @Input() id: string;
   @Input() label: string;
   @Input() iconClass: string;
   @Input() selected: boolean;
   @Output() select = new EventEmitter<MenuItem>();
 
+  @ContentChild(TemplateRef)
+  private customContentTpl: TemplateRef<any>;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterContentInit() {
+
+  }
+
+  get hasCustomTemplate(): boolean {
+    return this.customContentTpl !== undefined;
+  }
+
   didMenuItemClick(evt) {
+    console.log(this.customContentTpl);
     const menuItem = MenuItem.create({
       id: this.id,
       label: this.label
