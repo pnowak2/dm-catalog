b3444cd659436a28e153e32fb59cdd97a3f32309
Piotr Nowak <p.nowak2@gmail.com>
2017-08-31 20:07:12 +0200

    done

--- integrations/angular/src/app/components/menu/menu/menu.component.ts
+++ integrations/angular/src/app/components/menu/menu/menu.component.ts
@@ -1,19 +1,27 @@
-import { Component, AfterContentInit, ContentChildren, ContentChild, TemplateRef, QueryList, EventEmitter, Output, Input } from '@angular/core';
+import { Component, AfterContentInit, OnDestroy, ViewContainerRef, ComponentFactoryResolver, ContentChildren, ContentChild, ViewChild, TemplateRef, QueryList, EventEmitter, Output, Input } from '@angular/core';
 import { MenuItemComponent } from './../menu-item/menu-item.component';
 import { MenuItem } from './../menu-item/interface/menu-item';
 import { MenuModel } from './model/menu.model';
-
 @Component({
   selector: 'dm-menu',
   templateUrl: './menu.component.html'
 })
-export class MenuComponent implements AfterContentInit {
+export class MenuComponent implements AfterContentInit, OnDestroy {
   @Input() menuItems: Array<MenuItem>;
+  @ViewChild('dynamicContainer', { read: ViewContainerRef}) dynamicContainer: ViewContainerRef;
   @ContentChild(TemplateRef, { read: TemplateRef }) itemTemplate; 
   @ContentChildren(MenuItemComponent) contentItemComponents = new QueryList<MenuItemComponent>();
   @Output() select = new EventEmitter<MenuItem>();
 
+  constructor(private resolver: ComponentFactoryResolver) {  }
+
   didMenuItemSelect(menuItem: MenuItem) {
+    const factory = this.resolver.resolveComponentFactory(MenuItemComponent);
+    const compRef = this.dynamicContainer.createComponent(factory);
+    compRef.instance.label = 'boo';
+    compRef.instance.iconClass = 'icon-home2';
+    compRef.instance.select.subscribe(this.didMenuItemSelect.bind(this));
+
     this.select.next(menuItem);
     this.vm.selectNext();
   }
@@ -28,5 +36,8 @@ export class MenuComponent implements AfterContentInit {
     this.contentItemComponents.forEach(menuItem => {
       menuItem.select.subscribe(this.didMenuItemSelect.bind(this));
     });
+  }
+
+  ngOnDestroy() {
   }
 }
--- integrations/angular/src/app/components/menu/menu/menu.module.ts
+++ integrations/angular/src/app/components/menu/menu/menu.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu.component';
 import { MenuItemModule } from './../menu-item/menu-item.module';
+import { MenuItemComponent } from './../menu-item/menu-item.component';
 
 @NgModule({
   imports: [CommonModule, MenuItemModule],
   exports: [MenuComponent],
-  declarations: [MenuComponent]
+  declarations: [MenuComponent],
+  entryComponents: [MenuItemComponent]
 })
 export class MenuModule { }
